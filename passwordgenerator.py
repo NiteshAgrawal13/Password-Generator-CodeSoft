ada861da759e0ee8b064391b55610cc790102a08
NiteshAgrawal13 <nitesh.21b0311084@abes.ac.in>
2023-12-09 14:18:05 +0530

    commit

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,22 +1,20 @@
-#STRONG PASSWORD GENERATOR
 import random
 import string
 
 def generate_password(length):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(length))
+    char_words= string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(char_words) for _ in range(length))
     return password
 
 def main():
     try:
-        desired_length = int(input("Enter the desired length for the password: "))
+        desired_length = int(input("Please enter the length of password\n"))
         if desired_length <= 0:
-            print("Please enter a length greater than 0.")
+            print("please enter length greater tha 0")
         else:
-            password = generate_password(desired_length)
-            print("Generated Password:", password)
+            password=generate_password(desired_length)
+            print("Generated Password is:\n",password)
     except ValueError:
-        print("Please enter a valid length (a positive integer).")
-
+        print("Enter valid length")
 if __name__ == "__main__":
     main()
